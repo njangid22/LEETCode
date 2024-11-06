32f03b2af2e39383d66ebba195868459a5ca4ca0
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2024-11-07 00:42:34 +0530

    Time: 3 ms (7.14%) | Memory: 8.9 MB (27.78%) - LeetSync

--- 1070-convert-to-base-2/convert-to-base-2.cpp
+++ 1070-convert-to-base-2/convert-to-base-2.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     string baseNeg2(int n) {
-        if (n == 0) return "0";  // Correct handling of the n = 0 case
-        
+        if (n == 0) return "0";  
         string result;
-        
         while (n != 0) {
             int remainder = n % -2;
             n /= -2;
-
             if (remainder < 0) {
                 remainder += 2;
                 n += 1;
             }
-
-            result = to_string(remainder) + result;  // Accumulate the binary digits
+            result = to_string(remainder) + result;  
         }
-
         return result;
     }
 };
