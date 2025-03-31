22bef42060a592d2c4f74773ec028523e8db33dd
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-03-31 20:16:14 +0530

    Time: 2 ms (9.92%) | Memory: 50.5 MB (40.69%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,9 +3,7 @@ class Solution {
         int n = nums.length;
         int l = 0;
         int r= n-1;
-        if(n==1){
-            return nums[0];
-        }
+    
         while(l<=r){
             if(l==n-1){
                 return nums[l];
