062b8c41aeaf321b0f476eb1338dde5359644739
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-03-31 22:21:44 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (43.38%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int[] anew = new int[n + m];
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int i = 0, j = 0, k = 0;
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0;
+        int high = x;
 
-        // Merge two sorted arrays into one
-        while (i < n && j < m) {
-            if (nums1[i] < nums2[j]) {
-                anew[k++] = nums1[i++];
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double) Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
             } else {
-                anew[k++] = nums2[j++];
+                low = partitionX + 1;
             }
         }
-
-        // Copy remaining elements from nums1, if any
-        while (i < n) {
-            anew[k++] = nums1[i++];
-        }
-
-        // Copy remaining elements from nums2, if any
-        while (j < m) {
-            anew[k++] = nums2[j++];
-        }
-
-        int size = anew.length;
-
-        // Calculate the median
-        if (size % 2 == 0) {
-            return (anew[size / 2 - 1] + anew[size / 2]) / 2.0;
-        } else {
-            return anew[size / 2];
-        }
+        return 0;
     }
 }
