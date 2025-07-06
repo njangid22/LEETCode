fe320900230b67cd942d613de6b7feeae631c85a
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-07-06 23:38:09 +0530

    Time: 20 ms (94.21%) | Memory: 16.2 MB (89.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,30 +4,27 @@ public:
         if (grid.empty() || grid[0].empty()) {
             return 0;
         }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        int island=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j]=='1'){
+                    island++;
+                    dfs(grid,i,j);
                 }
             }
         }
-        
-        return numIslands;
+        return island;
     }
-    
-private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
+    private:
+        void dfs(vector<vector<char>>& grid, int i, int j){
+            if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]!='1'){
+                return;
+            }
+            grid[i][j]='0';
+            dfs(grid,i+1,j);
+            dfs(grid,i,j+1);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j-1);
+
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
-    }
 };
