fc6e99c41c7401d71fedfc94ad33f3b9c39d1983
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-07-14 20:15:59 +0530

    Time: 6 ms (34.54%) | Memory: 17 MB (33.11%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     set<vector<int>> s;
-    void com(vector<int>& arr, int i ,vector<int>& comb, int target,vector<vector<int>>& ans){
-                if(arr.size()==i||target<0){
-                    return ;
-                }
-                if(target==0){
-                    if(s.find(comb)==s.end()){
-                     ans.push_back(comb);
-                     s.insert(comb);
-                    }
+    void com(vector<int>& candidates, int target, int i, vector<int>& comb, vector<vector<int>>& ans){
+            if(candidates.size()==i||target<0){
+                return;
+            }
+            if(target==0){
+                if(s.find(comb)==s.end()){
+                    ans.push_back(comb);
+                    s.insert(comb);
                     return;
                 }
-                comb.push_back(arr[i]);
-                com(arr, i+1, comb, target-arr[i], ans);
-                com(arr, i, comb, target-arr[i], ans);
-                comb.pop_back();
-                com(arr, i+1, comb, target, ans);
-                
             }
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+            comb.push_back(candidates[i]);
+            com(candidates, target-candidates[i], i+1, comb, ans);
+            com(candidates, target-candidates[i], i, comb, ans);
+            comb.pop_back();
+            com(candidates, target, i+1, comb, ans);
+            
+            
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> comb;
         vector<vector<int>> ans;
-        com(arr, 0, comb, target, ans);
+        com(candidates, target, 0, comb, ans);
         return ans;
     }
 };
