d13ad268d58c630ec3d25fb09c44bee9d2984e65
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-07-18 15:30:48 +0530

    Time: 16 ms (89.12%) | Memory: 17.9 MB (84.64%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, 1e9));
+        vector<int> dp(target + 1, 1e9);
+        dp[0] = 0;
         
-        for(int T = 0; T <= target; T++) {
-            if(T % nums[0] == 0) dp[0][T] = T / nums[0];
-            else dp[0][T] = 1e9; // handle cases where T is not divisible by nums[0]
-        }
-        
-        for(int ind = 1; ind < n; ind++) {
-            for(int T = 0; T <= target; T++) {
-                int notTake = dp[ind-1][T];
-                int take = 1e9;
-                if(nums[ind] <= T) {
-                    take = 1 + dp[ind][T - nums[ind]];
-                }
-                dp[ind][T] = min(take, notTake);
+        for(int i=0;i<nums.size();i++) {
+            for(int T = nums[i]; T <= target; T++) {
+                dp[T] = min(dp[T], 1 + dp[T - nums[i]]);
             }
         }
         
-        int ans = dp[n-1][target];
+        int ans = dp[target];
         if(ans >= 1e9) return -1;
         return ans;
     }
