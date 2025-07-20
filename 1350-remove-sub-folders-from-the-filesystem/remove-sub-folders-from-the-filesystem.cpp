0657f65bcc7bf5c3943f5cdfcab7bc06ead3a92e
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-07-20 10:42:23 +0530

    Time: 46 ms (85.14%) | Memory: 49.2 MB (74.95%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 class Solution {
 public:
-    vector<std::string> removeSubfolders(vector<string>& folder) {
+    vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(), folder.end());
-        vector<string> ans = {folder[0]};
-
+        vector<string> ans;
+        ans.push_back(folder[0]);
         for (int i = 1; i < folder.size(); ++i) {
             if (folder[i].substr(0, ans.back().size() + 1) != ans.back() + "/") {
                 ans.push_back(folder[i]);
