7fd1fa842804202f79cf250c75d0f90dd2c46422
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2025-09-05 20:50:08 +0530

    Time: 1 ms (94.29%) | Memory: 41.2 MB (84.14%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,24 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             if (num > 0) {
-                q.add(num);
+                set.add(num);
             }
         }
-        int operations = 0;
-        while (!q.isEmpty()) {
-            int min = q.poll();
-            operations++;
-            PriorityQueue<Integer> temp = new PriorityQueue<>();
-            while (!q.isEmpty()) {
-                int num = q.poll();
-                if (num - min > 0) {
-                    temp.add(num - min);
-                }
-            }
-            q = temp;
-        }
-        return operations;
+        return set.size();
     }
 }
